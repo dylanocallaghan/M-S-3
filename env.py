86f690803ac93b904b3e5d3f57d836b7bd2b5b11
dylanocallaghan <dylan.oc.1999@gmail.com>
2021-07-24 02:09:52 +0000

    added env.py so it doesnt reset my whole gitpod again will remove it later

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "BrN~104P!w?}!rcvIxc8_V$q3rqynU")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster.7akgp.mongodb.net/task_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DB", "task_manager")
